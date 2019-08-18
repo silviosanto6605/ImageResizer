b0c4eb02af07aed9a97fc5666e918f0f4f455c12
Silvio Santoriello <silviosanto6605@gmail.com>
2019-08-18 18:13:15 +0200

    Initial Commit

---
+++ ImageResize.py
@@ -0,0 +1,51 @@
+#pylint:disable=E0602
+#!usr/bin/env/python3
+
+try:
+    import PIL
+    from PIL import Image
+    import os
+
+    WannaGoOn = True
+
+    while WannaGoOn == True:
+
+        imageFile = input("Insert the image name ")
+        im1 = Image.open(imageFile)
+
+        largh = int(input("Inserisci la larghezza dell'immagine "))
+        altezza = int(input("Inserisci l'altezza dell'immagine "))
+
+        im2 = im1.resize((largh,altezza), Image.NEAREST)
+        ext = ".jpg"
+        Name = input("Salva con nome: ")
+        im2.save(Name + ext)
+        wanna_go_on = input("Vuoi continuare il ridimensionamento di altre immagini? ")
+        if wanna_go_on  == "Si" or "Sì":
+            WannaGoOn = True
+        elif wanna_go_on == "No":
+            WannaGoOn == False
+            exit(0)
+
+except MemoryError:
+    print("Errore di memoria")
+    exit(0)
+
+
+except FileNotFoundError:
+    print('File non trovato...')
+    exit(0)
+
+except ImportError:
+    os.system("python -m pip install Image")
+    print("I am installing the missing packages.")
+    exit(0)
+
+
+except KeyboardInterrupt:
+     print("Exit...")
+     exit(0)
+
+except OverflowError:
+    print("ERRORE DI MEMORIA! Termino...")
+    exit(0)
